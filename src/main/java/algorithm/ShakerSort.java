6389cc2beed0260e62a59a8611afce37e9b7c392
s1310173 <seiyan.0625m@gmail.com>
2026-01-15 15:31:28 +0900

    BubbleSortとShakerSortをAbstractSortAlgorithmから継承するように変更し、CliAppでのソートアルゴリズムの選択機能を追加しました。また、TestEngineクラスを新規作成し、配列がソートされているかを確認するメソッドを実装しました。さらに、.gitignoreファイルを追加しました。

--- src/main/java/algorithm/ShakerSort.java
+++ src/main/java/algorithm/ShakerSort.java
@@ -1,18 +1,21 @@
-public class ShakerSort {
+package algorithm;
 
-    public static void shakerSort(int[] a) {
+public class ShakerSort extends AbstractSortAlgorithm {
+
+    @Override
+    public void sort(int[] array) {
         int left = 0;
-        int right = a.length - 1;
+        int right = array.length - 1;
         boolean swapped;
 
         while (left < right) {
             swapped = false;
             // 左 → 右
             for (int i = left; i < right; i++) {
-                if (a[i] > a[i + 1]) {
-                    int temp = a[i];
-                    a[i] = a[i + 1];
-                    a[i + 1] = temp;
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
                     swapped = true;
                 }
             }
@@ -21,10 +24,10 @@ public class ShakerSort {
             swapped = false;
             // 右 → 左
             for (int i = right; i > left; i--) {
-                if (a[i] < a[i - 1]) {
-                    int temp = a[i];
-                    a[i] = a[i - 1];
-                    a[i - 1] = temp;
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
                     swapped = true;
                 }
             }
@@ -32,11 +35,9 @@ public class ShakerSort {
             if (!swapped) break;
         }
     }
-    public static void main(String[] args) {
-        int[] data = {5, 3, 8, 4, 2};
-        shakerSort(data);
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(data[i] + " ");
-        }
+
+    @Override
+    public String getName() {
+        return "Shaker Sort";
     }
 }
